9de43d7f9f66ebbcb4cad0dfcdccb907243821b7
Lucas Bartholemy <lucas@bartholemy.com>
2022-05-20 17:14:58 +0200

    Fixes Win CLI

--- tests/Cli/ConsoleCest.php
+++ tests/Cli/ConsoleCest.php
@@ -10,14 +10,14 @@ final class ConsoleCest
 {
     public function testCommandYii(CliTester $I): void
     {
-        $command = dirname(__DIR__, 2) . '/cmda';
+        $command = dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'cmda';
         $I->runShellCommand($command);
         $I->seeInShellOutput('Yii Console');
     }
 
     public function testCommandHello(CliTester $I): void
     {
-        $command = dirname(__DIR__, 2) . '/cmda';
+        $command = dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'cmda';
         $I->runShellCommand($command . ' hello');
         $I->seeInShellOutput('Hello!');
     }
